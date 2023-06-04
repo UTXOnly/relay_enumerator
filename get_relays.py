72722762a033fe9416e95a02c8e69fc6b5c993a7
Brian Hartford <bhartford419@gmail.com>
2023-06-04 17:05:06 -0400

    Add docker labgels

---
+++ get_relays.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.nostr.watch/v1/public"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    items_list = []
+    for item in data:
+        items_list.append(item)
+        print(f"Added item {item} to the list")
+    print(len(items_list))
+else:
+    print("Error: Unable to fetch data from API")
+
